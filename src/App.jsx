6aedab2362e584de56010241e4f4914aa2bde25d
zahid072 <jahidhasan19368@gmail.com>
2024-03-22 22:41:27 +0600

    nabvar added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Home from './components/home/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='max-w-[1280px] md:mx-auto mx-2'>
+        <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">CareerHub</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a href="">Statistics</a></li>
+      <li><a href="">Applied Jobs</a></li>
+      <li><a href="">Blog</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className=" px-4 py-2 bg-gradient-to-l from-purple-500 to-blue-400 text-white font-bold rounded-lg">Star Applying</a>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Navbar
